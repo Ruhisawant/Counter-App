f299f1df8e8cf9a513ec5a5788202299b6658c72
Ruhisawant <ruhi.p.sawant@gmail.com>
2025-02-18 17:01:38 -0500

    Fixed UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Stateful Widget',
+      title: 'Counter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -17,18 +18,21 @@ class MyApp extends StatelessWidget {
 }
 
 class CounterWidget extends StatefulWidget {
+  const CounterWidget({super.key});
+
   @override
-  _CounterWidgetState createState() => _CounterWidgetState();
+  CounterWidgetState createState() => CounterWidgetState();
 }
 
-class _CounterWidgetState extends State<CounterWidget> {
-  int _counter = 0; //initial couter value
+class CounterWidgetState extends State<CounterWidget> {
+  final List<int> _history = [];
+  final TextEditingController _controller = TextEditingController();
+  String _limitMessage = '';
+  int _counter = 0;
   int _value = 1;
+  int targetNum = 50;
   int min = 0;
   int max = 100;
-  String _limitMessage = '';
-  final List<int>_history = [];
-  int targetNum = 50;
 
   void incrementCounter() {
     setState(() {
@@ -56,6 +60,8 @@ class _CounterWidgetState extends State<CounterWidget> {
     setState(() {
       _counter = 0;
       _history.clear();
+      _limitMessage = '';
+      _value = 1;
     });
   }
 
@@ -67,108 +73,173 @@ class _CounterWidgetState extends State<CounterWidget> {
       });
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Stateful Widget'),
+        title: const Text('Counter App'),
+        backgroundColor: Colors.blueAccent,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              color: Colors.blue,
-              child: Column(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
                 children: [
                   Text(
                     '$_counter',
                     style: TextStyle(
                       fontSize: 50.0,
+                      fontWeight: FontWeight.bold,
                       color: _counter == 0
                           ? Colors.red
                           : (_counter >= 50 ? Colors.green : Colors.black),
                     ),
                   ),
-
-                  if (_limitMessage.isNotEmpty) 
+                  if (_limitMessage.isNotEmpty)
                     Text(
                       _limitMessage,
-                      style: const TextStyle(color: Colors.red),
+                      style: const TextStyle(color: Colors.red, fontSize: 16),
                     ),
-
-                  if (_counter == targetNum) 
+                  if (_counter == targetNum)
                     const Text(
                       'You\'ve reached the target number!',
                       style: TextStyle(color: Colors.red, fontSize: 16),
                     ),
                 ],
+              ),
+
+              const SizedBox(height: 30),
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: incrementCounter,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(150, 50),
+                          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 32.0),
+                          backgroundColor: Colors.blueAccent,
+                        ),
+                        child: Text('Add $_value', style: TextStyle(fontSize: 16, color: Colors.black)),
+                      ),
+                      
+                      const SizedBox(height: 10),
+                      
+                      ElevatedButton(
+                        onPressed: undoCounter,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(150, 50),
+                          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 32.0),
+                          backgroundColor: Colors.orangeAccent,
+                        ),
+                        child: const Text('Undo', style: TextStyle(fontSize: 16, color: Colors.black)),
+                      ),
+                    ],
+                  ),
+                  
+                  const SizedBox(width: 20),
+                  
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: decrementCounter,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(150, 50),
+                          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 32.0),
+                          backgroundColor: Colors.blueAccent,
+                        ),
+                        child: Text('Delete $_value', style: TextStyle(fontSize: 16, color: Colors.black)),
+                      ),
+                      
+                      const SizedBox(height: 10),
+                      
+                      ElevatedButton(
+                        onPressed: resetCounter,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(150, 50),
+                          padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 32.0),
+                          backgroundColor: Colors.redAccent,
+                        ),
+                        child: const Text('Reset', style: TextStyle(fontSize: 16, color: Colors.black)),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 30),
+              
+              TextField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  labelText: 'Add Custom Increment Value',
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                ),
+                keyboardType: TextInputType.number,
+                onSubmitted: (text) {
+                  if (text.isNotEmpty) {
+                    int enteredNumber = int.tryParse(text) ?? 0;
+                    setState(() {
+                      _value = enteredNumber;
+                    });
+                    _controller.clear();
+                  }
+                },
+              ),
+
+              const SizedBox(height: 20),
+
+              Slider(
+                min: 0,
+                max: 100,
+                value: _counter.toDouble(),
+                onChanged: (double value) {
+                  setState(() {
+                    _counter = value.toInt();
+                  });
+                },
+                activeColor: Colors.blueAccent,
+                inactiveColor: Colors.redAccent,
+              ),
+
+              const SizedBox(height: 20),
+              
+              Align(
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                  'History:',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              SizedBox(
+                height: 100, 
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _history.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        '${_history[index]}',
+                        style: TextStyle(fontSize: 25)
+                      ),
+                    );
+                  },
+                ),
               )
-            ),
+            ],
           ),
-
-          ElevatedButton(
-            onPressed: incrementCounter,
-            child: const Text('Add'),
-          ),
-
-          ElevatedButton(
-            onPressed: decrementCounter,
-            child: const Text('Delete'),
-          ),
-
-          ElevatedButton(
-            onPressed: resetCounter,
-            child: const Text('Reset'),
-          ),
-
-          ElevatedButton(
-            onPressed: undoCounter,
-            child: const Text('Undo'),
-          ),
-
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'Add Custom Increment Value',
-              border: OutlineInputBorder(),
-            ),
-            keyboardType: TextInputType.number,
-            onSubmitted: (text) {
-              if (text.isNotEmpty) {
-                int enteredNumber = int.tryParse(text) ?? 0;
-                setState(() {
-                  _value = enteredNumber;
-                });
-              }
-            },
-            controller: TextEditingController()..clear(),
-          ),
-
-          Slider(
-            min: 0,
-            max: 100,
-            value: _counter.toDouble(),
-            onChanged: (double value) {
-              setState(() {
-              _counter = value.toInt();
-              });
-            },
-            activeColor: Colors.blue,
-            inactiveColor: Colors.red,
-          ),
-
-          Expanded(
-            child: ListView.builder(
-              itemCount: _history.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('Values: ${_history[index]}'),
-                );
-              },
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
